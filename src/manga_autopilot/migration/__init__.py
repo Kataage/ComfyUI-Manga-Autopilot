de5bb4a8efb890f794e845341c1d30d67e70a559
Kataage <50143135+Kataage@users.noreply.github.com>
2026-09-20 04:05:15 +0900

    feat: add legacy migration package

---
+++ src/manga_autopilot/migration/__init__.py
@@ -0,0 +1,15 @@
+"""Legacy migration and import-discovery helpers."""
+
+from manga_autopilot.migration.legacy_inventory import (
+    LegacyInventoryEntry,
+    LegacyInventoryWarning,
+    LegacyProjectInventory,
+    LegacyProjectInventoryService,
+)
+
+__all__ = [
+    "LegacyInventoryEntry",
+    "LegacyInventoryWarning",
+    "LegacyProjectInventory",
+    "LegacyProjectInventoryService",
+]
